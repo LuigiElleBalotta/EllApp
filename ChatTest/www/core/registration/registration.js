8c7a7a8bb2c52c1d034a6c0395ce12ca77c4f8b0
Elle <lbalotta@transferoil.com>
2016-01-25 11:24:48 +0100

    Implement glypicon + Implement component 'Callout' + added registration page + script registration page + star login page

---
+++ ChatTest/www/core/registration/registration.js
@@ -0,0 +1,53 @@
+﻿$("document").ready(function ()
+{
+    $("#avviso").hide();
+
+    $("#inviaModuloCreazioneAccount").click(function (e)
+    {
+        e.preventDefault();
+        $("#inviaModuloCreazioneAccount").button("loading");
+        var username = $("#user").val().trim();
+        var password = $("#psw").val().trim();
+        var email = $("#email").val().trim();
+        var emailok = false;
+        if (isValidEmail(email))
+            emailok = true;
+        if (username == "" || password == "" || email == "") {
+            $("#inviaModuloCreazioneAccount").button("reset");
+            $("#risultato").html("Errore nella creazione dell'account");
+            $("#avviso").show(300);
+            setTimeout(resetAvviso, 3000);
+            return false;
+        }
+        else {
+            if (!emailok) {
+                $("#inviaModuloCreazioneAccount").button("reset");
+                $("#risultato").html("Email non valida");
+                $("#avviso").show(300);
+                setTimeout(resetAvviso, 3000);
+                return false;
+            }
+            $.post("http://ellapp.dev/registration/elaboraCreazione.php", "user=" + username + "&psw=" + password + "&email=" + email, function (data, status) {
+                var msg = "";
+                if (data == "1")
+                    msg = "Account creato con successo!";
+                else
+                    msg = "Errore";
+                $("#risultato").html(msg);
+                $("#avviso").show(300);
+                $("#inviaModuloCreazioneAccount").button("reset");
+                setTimeout(resetAvviso, 3000);
+            });
+        }
+    });
+
+    function resetAvviso() {
+        $("#avviso").hide(300);
+        $("#risultato").html("");
+    }
+
+    function isValidEmail(emailAddress) {
+        var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+        return pattern.test(emailAddress);
+    };
+});
