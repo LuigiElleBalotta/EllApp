f685101bb5e5e4259ff0ca10798b76b472cd5f8d
Elle <lbalotta@transferoil.com>
2016-01-27 15:51:28 +0100

    Top Menù

---
+++ ChatTest/www/scripts/responsive_menu.js
@@ -0,0 +1,23 @@
+﻿$("document").ready(function ()
+{
+    collapseNavbar();
+    // jQuery to collapse the navbar on scroll
+    function collapseNavbar() {
+        if ($(".navbar").offset().top > 50) {
+            $(".navbar-fixed-top").addClass("top-nav-collapse");
+        } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        }
+    }
+
+    $(window).scroll(collapseNavbar);
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function (e) {
+        if (hasClass(e.target, "nav-page")) {
+            $('.navbar-toggle:visible').click();
+        }
+    });
+    function hasClass(elem, className) {
+        return elem.className.split(' ').indexOf(className) > -1;
+    }
+});
