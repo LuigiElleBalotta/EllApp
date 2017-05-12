a155968cbd4f261818ac8c2632aeec3e96a29f38
Luigi <luigi.balotta@exelis.it>
2017-05-12 09:52:50 +0200

    Re-Sharper things

--- ServerStuff/EllApp_server/Network/Handlers/MessageHandler.cs
+++ ServerStuff/EllApp_server/Network/Handlers/MessageHandler.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Alchemy;
 using Alchemy.Classes;
 using EllApp_server.Classes;
 using EllApp_server.definitions;
@@ -14,23 +11,23 @@ namespace EllApp_server.Network.Handlers
 	public class MessageHandler
 	{
 		[System.Diagnostics.Conditional("DEBUG")]
-		public void HandleMessage(UserContext aContext, dynamic obj, List<Session> Sessions)
+		public void HandleMessage(UserContext aContext, dynamic obj, List<Session> sessions)
 		{
 			Console.WriteLine("MESSAGE PACKET FROM " + aContext.ClientAddress);
 			string messagecontent = obj.Message;
-			ChatType to_type = obj.ToType;
+			ChatType toType = obj.ToType;
 			int from = obj.From;
 			int to = obj.To;
 
-			MakeLog(from, to, to_type, messagecontent);
+			MakeLog(from, to, toType, messagecontent);
 
-			switch (to_type)
+			switch (toType)
 			{
 				case ChatType.CHAT_TYPE_GLOBAL_CHAT: //Send message to all connected clients (that we have stored in sessions)
-					HandleGlobalChat(Sessions, from, to, to_type, messagecontent);
+					HandleGlobalChat(sessions, from, to, toType, messagecontent);
 					break;
 				case ChatType.CHAT_TYPE_USER_TO_USER:
-					HandleUserChat(Sessions, obj);
+					HandleUserChat(sessions, obj);
 					break;
 				case ChatType.CHAT_TYPE_GROUP_CHAT:
 					HandleGroupChat();
@@ -41,49 +38,51 @@ namespace EllApp_server.Network.Handlers
 			}
 		}
 
-		private void MakeLog(int from, int to, ChatType to_type, string messagecontent)
+		private void MakeLog(int from, int to, ChatType toType, string messagecontent)
 		{
-			var log = new Log_Manager();
-			log.ChatID = Misc.CreateChatRoomID(from, to);
-			log.content = messagecontent;
-			log.to_type = to_type;
-			log.from = from;
-			log.to = to;
-			log.SaveLog();
+			new Log_Manager
+					  {
+						  ChatID = Misc.CreateChatRoomID(from, to),
+						  content = messagecontent,
+						  to_type = toType,
+						  from = from,
+						  to = to
+					  }.SaveLog();
 		}
 
-		private void HandleGlobalChat(List<Session> Sessions, int from, int to, ChatType to_type, string messagecontent)
+		private void HandleGlobalChat(List<Session> sessions, int from, int to, ChatType toType, string messagecontent)
 		{
-			var StCLog = new Log_Manager();
+			var stCLog = new Log_Manager();
 			var o = 1;
-			foreach (var session in Sessions)
+			foreach (var session in sessions)
 			{
 				if (session.GetUser().GetID() != from) //Do not send message to ourselves
 				{
 					Chat c = new Chat(ChatType.CHAT_TYPE_GLOBAL_CHAT, Misc.CreateChatRoomID(from, session.GetUser().GetID()), messagecontent, Misc.GetUsernameByID(from), Misc.GetUsernameByID(session.GetUser().GetID()));
 					session.SendMessage(new MessagePacket(MessageType.MSG_TYPE_CHAT, from, session.GetUser().GetID(), JsonConvert.SerializeObject(c)));
-					StCLog.content = messagecontent;
-					StCLog.to_type = to_type;
-					StCLog.from = from;
-					StCLog.to = session.GetUser().GetID();
-					StCLog.SaveLog();
+					stCLog.content = messagecontent;
+					stCLog.to_type = toType;
+					stCLog.from = from;
+					stCLog.to = session.GetUser().GetID();
+					stCLog.SaveLog();
 					o++;
 				}
 			}
 			Console.WriteLine("Message sent to {0} users", (o - 1));
 		}
 
-		private void HandleUserChat(List<Session> Sessions, dynamic obj)
+		private void HandleUserChat(List<Session> sessions, dynamic obj)
 		{
 			Console.WriteLine("Received MSG_TYPE_CHAT_WITH_USER packet.");
 			//If the receiving User is online, we can send the message to him, otherwise he will load everything at next login
-			if (Sessions.Any(s => s.GetUser().GetID() == (int)obj.To))
+			if (sessions.Any(s => s.GetUser().GetID() == (int)obj.To))
 			{
-				if (Sessions.SingleOrDefault(s => s.GetUser().GetID() == (int)obj.To).GetUser().IsOnline())
+				Session singleOrDefault = sessions.SingleOrDefault(s => s.GetUser().GetID() == (int)obj.To);
+				if (singleOrDefault != null && singleOrDefault.GetUser().IsOnline())
 				{
 					Chat c = new Chat(ChatType.CHAT_TYPE_USER_TO_USER, Misc.CreateChatRoomID(obj.To, obj.From), obj.Message, obj.From, obj.To);
-					Session session = Sessions.SingleOrDefault(s => s.GetUser().GetID() == (int)obj.To);
-					session.SendMessage(new MessagePacket(MessageType.MSG_TYPE_CHAT, obj.From, obj.To, JsonConvert.SerializeObject(c)));
+					Session session = sessions.SingleOrDefault(s => s.GetUser().GetID() == (int)obj.To);
+					session?.SendMessage(new MessagePacket(MessageType.MSG_TYPE_CHAT, obj.From, obj.To, JsonConvert.SerializeObject(c)));
 				}
 			}
 			else
