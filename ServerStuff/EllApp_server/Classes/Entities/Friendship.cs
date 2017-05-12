ba656b6914c551a71cf3fc589555174673d35992
Luigi <luigi.balotta@exelis.it>
2017-05-12 10:39:20 +0200

    Add Friendship entity

---
+++ ServerStuff/EllApp_server/Classes/Entities/Friendship.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EllApp_server.Classes.Entities
+{
+	public class Friendship
+	{
+		public string person1 { get; set; }
+		public string person2 { get; set; }
+		public bool isPending { get; set; }
+	}
+}
