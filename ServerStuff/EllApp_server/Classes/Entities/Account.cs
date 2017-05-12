15cc2f1a2b9ba2a4454e243fc13d56c3b8d0f4eb
Luigi <luigi.balotta@exelis.it>
2017-05-12 10:33:52 +0200

    Added account entity

---
+++ ServerStuff/EllApp_server/Classes/Entities/Account.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EllApp_server.Classes.Entities
+{
+	public class Account
+	{
+		public int		idAccount { get; set; }
+		public string	username { get; set; }
+		public string	password { get; set; }
+		public string	email { get; set; }
+		public DateTime data_creazione { get; set; }
+		public string	last_connection { get; set; }
+		public string	last_ip { get; set; }
+		public bool		isOnline { get; set; }
+	}
+}
