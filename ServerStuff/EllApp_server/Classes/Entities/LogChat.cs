8c83cebc9517c05ae65a607e54116f71193e7b19
Luigi <luigi.balotta@exelis.it>
2017-05-12 11:05:51 +0200

    add LogChat entity

---
+++ ServerStuff/EllApp_server/Classes/Entities/LogChat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EllApp_server.definitions;
+
+namespace EllApp_server.Classes.Entities
+{
+	public class LogChat
+	{
+		public int		idLog { get; set; }
+		public string	content { get; set; }
+		public int		from { get; set; }
+		public ChatType toType { get; set; }
+		public int		to { get; set; }
+	}
+}
