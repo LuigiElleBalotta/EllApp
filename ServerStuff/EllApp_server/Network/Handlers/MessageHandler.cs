2f73718a6d3e9813072820281c535e7e9355868e
Luigi <luigi.balotta@exelis.it>
2017-05-12 16:29:18 +0200

    Remove unusued using

--- ServerStuff/EllApp_server/Network/Handlers/MessageHandler.cs
+++ ServerStuff/EllApp_server/Network/Handlers/MessageHandler.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Alchemy.Classes;
 using EllApp_server.Classes;
 using EllApp_server.definitions;
-using Newtonsoft.Json;
 using NLog;
 
 namespace EllApp_server.Network.Handlers
